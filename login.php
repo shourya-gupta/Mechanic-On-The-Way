d9c91ea15a778003fe74c38b4ecf61cddb659c5a
MihirAntwal <71126465+MihirAntwal@users.noreply.github.com>
2021-04-03 22:43:42 +0530

    Uploaded the login page

---
+++ login.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login in</title>
+<link rel="stylesheet" type="text/css" href="style2.css" />
+</head>
+<body>
+<div class="container">
+  <section id="content">
+    <form action="login_verification.php" method="post">
+      <h1>Login</h1>
+      <div>
+        <input type="text" placeholder="Email" id="email" name="email" required>
+      </div>
+      <div>
+        <input type="password" placeholder="Password" id="pass" name="pass" required >
+      </div>
+      <div>
+        <input type="submit" value="Log in"/>
+        <a href="forgot1.html">Forgot password</a>
+        <a href="signup.php">Sign up</a>
+      </div>
+    </form><!-- form -->
+
+  </section><!-- content -->
+</div><!-- container -->
+</body>
+</html>
