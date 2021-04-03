19e496365d84c03ee17b781711e6c0912b52e786
Spartan037 <68962530+Spartan037@users.noreply.github.com>
2021-04-04 02:25:56 +0530

    Update mechanic.sql

--- db/mechanic.sql
+++ db/mechanic.sql
@@ -1,1 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2021 at 10:54 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mechanic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency`
+--
+
+CREATE TABLE `emergency` (
+  `name` varchar(250) NOT NULL,
+  `phone` varchar(250) NOT NULL,
+  `landmark` varchar(250) NOT NULL,
+  `description` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `general`
+--
+
+CREATE TABLE `general` (
+  `descrip` varchar(250) NOT NULL,
+  `date1` varchar(250) NOT NULL,
+  `time1` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+
+CREATE TABLE `signup` (
+  `full_name` varchar(250) NOT NULL,
+  `mob_no` varchar(250) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `pass` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `signup`
+--
+
+INSERT INTO `signup` (`full_name`, `mob_no`, `address`, `email`, `pass`) VALUES
+('Ridhika', '6003501683', 'Madhya Pradesh', 'ridhika@gmail.com', 'Ridhi@123'),
+('Sunrit', '9876767676', 'Assam', 'sunrit@gmail.com', 'Sunrit@1234');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
