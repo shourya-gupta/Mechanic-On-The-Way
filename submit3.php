950fe12e67349745a13fcf9d3f74cbe441ec066b
Spartan037 <68962530+Spartan037@users.noreply.github.com>
2021-04-04 02:21:54 +0530

    Update submit3.php

--- submit3.php
+++ submit3.php
@@ -1,4 +1,4 @@
 <?php
       echo "<h1 align='center'> FORM SUBMITTED SUCCESSFULLY!!!! </h1>";
-      	header("Refresh:3; url=index.php",  true, 303);
+      	header("Refresh:3; url=index.html",  true, 303);
 ?>
